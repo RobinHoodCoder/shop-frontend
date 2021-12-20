50b2ad7bd24d2574ab84b4ab533fc51d0ea2bd40
PublicCodeRobin <86063078+PublicCodeRobin@users.noreply.github.com>
2021-12-20 16:27:09 +0100

    remove from cart.. Cache

--- components/CartButtons/RemoveFromCartButton/RemoveFromCartButton.jsx
+++ components/CartButtons/RemoveFromCartButton/RemoveFromCartButton.jsx
@@ -1,21 +1,22 @@
 import { useMutation } from '@apollo/client';
 import { M_REMOVE_FROM_CART } from '../../../gql/mutations';
-import { Q_CURRENT_USER } from '../../../gql/queries';
 import { useCart } from '../../../context/CartState';
 import styled from 'styled-components';
 
 const StyledButton = styled.button`
-font-size: 1rem;
-background: none;
-border: 0;
-  
+  font-size: 1rem;
+  background: none;
+  border: 0;
 `;
 
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteCartItem));
+}
 export const RemoveFromCartButton = ({ id }) => {
   const { openCart } = useCart();
   const [addToCart, { loading, error }] = useMutation(M_REMOVE_FROM_CART, {
     variables: { id },
-    refetchQueries: [{ query: Q_CURRENT_USER }],
+    update,
   });
   const handleAddToCart = () => {
     addToCart().then(() => openCart());
